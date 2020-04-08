24ba9ebcf8ed4a386e292654d3dc170fc8cc950d
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-04-08 14:38:09 +0530

    REFACTORED: Code to accomodate to dry principle for sorting

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -3,6 +3,7 @@ package censusanalyser;
 import com.bl.csvbuilder.CsvBuilder;
 import com.bl.csvbuilder.CsvFileBuilderException;
 import com.bl.csvbuilder.IscvBuilder;
+import com.google.gson.Gson;
 import com.opencsv.ICSVParser;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
@@ -11,17 +12,19 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
+    List<IndiaCensusCSV> csvFileList = null;
     public int loadIndiaCensusData(String csvFilePath) throws CesusAnalyserException {
         if (!csvFilePath.contains(".csv"))
             throw new CesusAnalyserException("Invalid file type", CesusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
             IscvBuilder CSVBuilderFactory = censusanalyser.CSVBuilderFactory.getCSVBuilder();
-            List<IndiaCensusCSV> csvFileList = CSVBuilderFactory.getCSVFileList(reader, IndiaCensusCSV.class);
+            csvFileList = CSVBuilderFactory.getCSVFileList(reader, IndiaCensusCSV.class);
             return csvFileList.size();
         } catch (IOException e) {
             throw new CesusAnalyserException(e.getMessage(),
@@ -40,7 +43,7 @@ public class CensusAnalyser {
             throw new CesusAnalyserException("Invalid file type", CesusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));){
             IscvBuilder CSVBuilderFactory = censusanalyser.CSVBuilderFactory.getCSVBuilder();
-            List<IndiaStateCodeCSV> csvFileList = CSVBuilderFactory.getCSVFileList(reader, IndiaStateCodeCSV.class);
+            csvFileList = CSVBuilderFactory.getCSVFileList(reader, IndiaStateCodeCSV.class);
             return csvFileList.size();
         } catch (IOException e) {
             throw new CesusAnalyserException(e.getMessage(),
@@ -59,4 +62,31 @@ public class CensusAnalyser {
         int numOfEnteries = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
         return numOfEnteries;
     }
+    public String getStateWiseSortedCensusData(String csvFilePath) throws CesusAnalyserException {
+        loadIndiaCensusData(csvFilePath);
+        if (csvFileList == null || csvFileList.size() == 0) {
+            throw new CesusAnalyserException("Invalid file data type", CesusAnalyserException.ExceptionType.INVALID_FILE_DATA_TYPE);
+        }
+
+        Comparator<IndiaCensusCSV> censusComparator = Comparator.comparing(census -> census.state);
+        this.sort(csvFileList, censusComparator);
+        String toJson = new Gson().toJson(csvFileList);
+        return toJson;
+    }
+
+    private void sort(List<IndiaCensusCSV> csvFileList, Comparator<IndiaCensusCSV> censusComparator) {
+        for (int i = 0; i < csvFileList.size(); i++) {
+            for (int j = 0; j < csvFileList.size() - i - 1; j++) {
+                IndiaCensusCSV census1 = csvFileList.get(j);
+                IndiaCensusCSV census2 = csvFileList.get(j + 1);
+                if (censusComparator.compare(census1, census2) > 0) {
+                    csvFileList.set(j, census2);
+                    csvFileList.set(j + 1, census1);
+                }
+
+            }
+
+        }
+    }
+
 }
